0bc064ee47935ae8ee1ae128df6b56acd5dd4b34
Gagan Singh <gagansingh.dev@outlook.com>
2020-07-05 19:13:22 -0400

    Implemented basic redux state management

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,39 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import * as actionTypes from "../../store/actions";
 import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
 class Counter extends Component {
-   state = {
-      counter: 0,
-   };
-
-   counterChangedHandler = (action, value) => {
-      switch (action) {
-         case "inc":
-            this.setState((prevState) => {
-               return { counter: prevState.counter + 1 };
-            });
-            break;
-         case "dec":
-            this.setState((prevState) => {
-               return { counter: prevState.counter - 1 };
-            });
-            break;
-         case "add":
-            this.setState((prevState) => {
-               return { counter: prevState.counter + value };
-            });
-            break;
-         case "sub":
-            this.setState((prevState) => {
-               return { counter: prevState.counter - value };
-            });
-            break;
-      }
-   };
-
    render() {
       return (
          <div>
@@ -51,6 +23,20 @@ class Counter extends Component {
                label="Subtract 15"
                clicked={this.props.onSubtractCounter}
             />
+            <hr />
+            <button onClick={() => this.props.onStoreResult(this.props.ctr)}>
+               Store Result
+            </button>
+            <ul>
+               {this.props.storedResults.map((strResult) => (
+                  <li
+                     key={strResult.id}
+                     onClick={() => this.props.onDeleteResult(strResult.id)}
+                  >
+                     {strResult.value}
+                  </li>
+               ))}
+            </ul>
          </div>
       );
    }
@@ -58,16 +44,22 @@ class Counter extends Component {
 
 const mapStateToProps = (state) => {
    return {
-      ctr: state.counter,
+      ctr: state.ctr.counter,
+      storedResults: state.res.results,
    };
 };
 
 const mapDispatchToProps = (dispatch) => {
    return {
-      onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
-      onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
-      onAddCounter: () => dispatch({ type: "ADD", val: 10 }),
-      onSubtractCounter: () => dispatch({ type: "SUBTRACT", val: 15 }),
+      onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+      onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+      onAddCounter: () => dispatch({ type: actionTypes.ADD, val: 10 }),
+      onSubtractCounter: () =>
+         dispatch({ type: actionTypes.SUBTRACT, val: 15 }),
+      onStoreResult: (result) =>
+         dispatch({ type: actionTypes.STORE_RESULT, result: result }),
+      onDeleteResult: (id) =>
+         dispatch({ type: actionTypes.DELETE_RESULT, resultElId: id }),
    };
 };
 
